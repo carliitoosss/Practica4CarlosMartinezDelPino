aba73bd57d2e498780f415e7fc662ff5f87f22a1
Carlos Martinez Del Pino <carlosmartinezdelpino1@gmail.com>
2026-02-16 11:03:18 +0100

    Inicio proyecto Practica 4

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -2,4 +2,8 @@ package com.ejemplo.p4;
 
 public class Main {
 
+	public static void main(String[] args) {
+		System.out.println("Practica 4 - Git casos reales");
+		System.out.println("Carlos Martínez Del Pino");
+	}
 }
