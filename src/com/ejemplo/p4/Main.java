63b0398e22ccf162c204dd49b229a0dbaf9d56f6
Carlos Martinez Del Pino <carlosmartinezdelpino1@gmail.com>
2026-02-24 10:51:48 +0100

    Error corregido en el punto 8.(Mensajes de eclipse)

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -5,7 +5,5 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Practica 4 - Git casos reales");
 		System.out.println("Carlos Martínez Del Pino");
-		System.out.println("Mensaje añadido en el punto 3.1");
-		System.out.println("Nueva linea de aprueba. Punto 4.1");
 	}
 }
