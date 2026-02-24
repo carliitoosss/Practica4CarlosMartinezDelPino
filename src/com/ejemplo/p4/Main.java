3efa6ba8f6a8f2c61ca16869941932b6ceed2614
Carlos Martinez Del Pino <carlosmartinezdelpino1@gmail.com>
2026-02-24 11:00:59 +0100

    Commit para perder y recuperar

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -5,5 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Practica 4 - Git casos reales");
 		System.out.println("Carlos Martínez Del Pino");
+		System.out.println("Mensaje añadido en el punto 9 - REFLOG");
 	}
 }
